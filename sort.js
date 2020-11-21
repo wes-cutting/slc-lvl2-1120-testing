c443056115c523a5f69543ed8259653d7786a331
Wes Cutting <wesleycutting@gmail.com>
2020-11-20 19:06:11 -0700

    Implemented Sort test

---
+++ sort.js
@@ -0,0 +1,6 @@
+function sort(input){
+    const sorted = [...input];
+    sorted.sort();
+    return sorted;
+}
+module.exports = sort;
---
+++ sort.test.js
@@ -0,0 +1,12 @@
+const { TestScheduler } = require('jest');
+const sort = require('./sort');
+
+describe('Sorting Small Arrays', () => {
+    test('five single digit sort', ()=>{
+        const input = [ 4, 3, 9, 2, 8 ];
+        const expected = [ 2, 3, 4, 8, 9 ];
+        const results = sort(input);
+        expect(results).toEqual(expected);
+    });
+    // You should have at least 12 test cases for your Algorithm
+});
